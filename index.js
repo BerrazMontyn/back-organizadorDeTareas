2b8cf799bf8e56dbd48c671d9eb3e2779f4afe05
diamecjs <diame.cjs@gmail.com>
2023-11-28 10:09:50 -0300

    arreglo modelos y base de datos

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-require('dotenv').config();
 
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at, process.env.PORT'); // eslint-disable-line no-console
+// Agregar un manejador de errores para sync
+conn.sync({ force: true })
+  .then(() => {
+    console.log('Database synchronized successfully');
+
+    // Tu código para iniciar el servidor aquí
+    server.listen(3001, () => {
+      console.log('%s listening at 3001'); // eslint-disable-line no-console
+    });
   });
-});
--- src/models/Task.js
+++ src/models/Task.js
@@ -1,10 +1,8 @@
-const { DataTypes } = require("sequelize");
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const { DataTypes, UUIDV4 } = require('sequelize');
+
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define("tasks", {
-    id: {
+  const Task = sequelize.define('Task', {
+    id:{
       type: DataTypes.UUID,
       defaultValue: UUIDV4,
       allowNull: false,
@@ -24,4 +22,6 @@ module.exports = (sequelize) => {
       type: DataTypes.DATE,
     },
   });
+
+  return Task;
 };
