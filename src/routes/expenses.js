07b05121c878c59e2ba75765da89756b92505a3c
diamecjs <diame.cjs@gmail.com>
2023-11-28 22:05:41 -0300

    ruta get expenses

--- src/routes/expenses.js
+++ src/routes/expenses.js
@@ -6,18 +6,44 @@ const { Expense } = require("../db");
 const router = Router();
 
 router.post("/", async (req, res) => {
-  const { name, amount} = req.body;
-  try {
-    const expense = await Expense.create({
-      name,
-      amount,
-    });
+    const { name, amount, image } = req.body;
+    try {
+        const expense = await Expense.create({
+            name,
+            amount,
+            image,
+        });
 
-    return res.status(200).send(expense);
-  } catch (error) {
-    console.error("Error en post expenses", error);
-    return res.status(500).send({ message: "Error en el servidor" });
-  }
+        return res.status(200).send(expense);
+    } catch (error) {
+        console.error("Error en post expenses", error);
+        return res.status(500).send({ message: "Error en el servidor" });
+    }
+});
+
+router.get("/", async (req, res) => {
+    try {
+        const expenses = await Expense.findAll(); {
+            where: { name: expenses }
+        }
+        if (!expenses) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No se encontraron gastos',
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            msg: 'Lista de gastos',
+            expenses,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error en get expenses',
+        });
+    }
 });
 
 module.exports = router;
